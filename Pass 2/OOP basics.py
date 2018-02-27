5e55d9ec37cc92c1d8894c44497758ca37e5e9a8
WillJobe <william.jobe@gmail.com>
2018-02-27 16:26:38 +0100

    Scope example

---
+++ Pass 2/OOP basics.py
@@ -0,0 +1,12 @@
+class MyClass:
+    #My first class
+    i = 12345
+    str = "Something"
+
+    def myFunc(self):
+        return 'hello world'
+
+x = MyClass()
+print(x.i)
+print(x.str)
+print(x.myFunc())
