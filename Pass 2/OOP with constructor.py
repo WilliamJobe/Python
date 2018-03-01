6dd98d94cb9be6d166c55791b3133c7776f4136c
WillJobe <william.jobe@gmail.com>
2018-03-01 15:00:45 +0100

    Updated

--- Pass 2/OOP with constructor.py
+++ Pass 2/OOP with constructor.py
@@ -1,7 +1,11 @@
 class MyMath:
+    myInt = 12345 #class variable the same for all instances
+    
     def __init__(self, x, y):
-        self.x = x
+        self.x = x #instance variables
         self.y = y
 
-test = MyMath(3.14,2.718)
-print(test.x,test.y)
+test1 = MyMath(3.14,2.718)
+test2 = MyMath(42,1337)
+print(test1.x,test1.y,test1.myInt)
+print(test2.x,test2.y,test2.myInt)
