eef119e923f6b12470fdd11fb5a3d3a3483407e8
WillJobe <william.jobe@gmail.com>
2018-03-13 16:14:42 +0100

    new file

---
+++ Pass 2/struct.py
@@ -0,0 +1,11 @@
+class Car:
+    pass
+
+myCar = Car()  # Create an empty employee record
+
+# Fill the fields of the record
+myCar.model = 'V 70'
+myCar.brand = 'Volvo'
+myCar.year = 2005
+
+print(myCar.model)
